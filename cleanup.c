c4b6932d0e3388e19d0ee8a6336ccefa0c0e048b
Sophia Koulen <sophia.koulen@outlook.com>
2023-01-08 14:47:33 +0100

    reordered functions in cleanup.c

--- cleanup.c
+++ cleanup.c
@@ -6,11 +6,23 @@
 /*   By: skoulen <skoulen@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 13:47:35 by skoulen           #+#    #+#             */
-/*   Updated: 2023/01/03 15:30:31 by skoulen          ###   ########.fr       */
+/*   Updated: 2023/01/08 14:46:24 by skoulen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+static void	cleanup_pids(int *pids);
+static void	cleanup_statuses(int *statuses);
+
+void	cleanup_pipex(t_pipex *p)
+{
+	cleanup_pipes(p->n_pipes, p->pipes);
+	cleanup_cmds(p->n_cmds, p->cmds);
+	cleanup_paths(p->n_cmds, p->paths);
+	cleanup_pids(p->pids);
+	cleanup_statuses(p->statuses);
+}
 
 static void	cleanup_pids(int *pids)
 {
@@ -20,13 +32,4 @@ static void	cleanup_pids(int *pids)
 static void	cleanup_statuses(int *statuses)
 {
 	free(statuses);
-}
-
-void	cleanup_pipex(t_pipex *p)
-{
-	cleanup_pipes(p->n_pipes, p->pipes);
-	cleanup_cmds(p->n_cmds, p->cmds);
-	cleanup_paths(p->n_cmds, p->paths);
-	cleanup_pids(p->pids);
-	cleanup_statuses(p->statuses);
 }
