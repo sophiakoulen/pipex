8c2565074c98a894965fa14d0e6f98b0e7b127b0
Sophia Koulen <sophia.koulen@outlook.com>
2023-01-08 12:09:57 +0100

    Same thing, less lines of code

--- splitter.c
+++ splitter.c
@@ -6,59 +6,38 @@
 /*   By: skoulen <skoulen@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 17:10:23 by skoulen           #+#    #+#             */
-/*   Updated: 2023/01/05 16:35:52 by skoulen          ###   ########.fr       */
+/*   Updated: 2023/01/08 12:07:26 by skoulen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-static char	*alloc_word(char *str)
+static char	*get_word(char *str)
 {
-	char	*word;
+	int		i;
+	char	*c;
 	int		len;
+	char	*word;
 
-	str = trim(str);
+	str = trim(str); //do i need trim?
 	len = word_len(str);
 	if (len)
 	{
 		word = malloc((len + 1) * sizeof(*word));
-		return (word);
+		if (word)
+		{
+			i = 0;
+			while (i < len)
+			{
+				c = iter(&str);
+				word[i] = *c;
+				i++;
+			}
+			word[i] = 0;
+			return (word);
+		}
 	}
 	return (0);
-}
-
-static void	fill_word(char *str, char *word)
-{
-	int		i;
-	char	*c;
-
-	str = trim(str);
-	i = 0;
-	while (1)
-	{
-		c = iter(&str);
-		if (!c)
-		{
-			word[i] = 0;
-			break ;
-		}
-		else
-		{
-			word[i] = *c;
-		}
-		i++;
-	}
-}
-
-static char	*get_word(char *str)
-{
-	char	*word;
-
-	word = alloc_word(str);
-	if (!word)
-		return (0);
-	fill_word(str, word);
-	return (word);
 }
 
 static int	word_count(char *str)
